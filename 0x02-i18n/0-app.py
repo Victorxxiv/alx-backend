5fb5c84a81e52076676e5936a177cf89807359f3
Victor Mwendwa <victormwendwa804@gmail.com>
2024-07-16 20:11:50 +0300

    First Commit

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Basic Flask app
+"""
+
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index():
+    """
+    Hello World
+    """
+    return render_template('0-index.html')
