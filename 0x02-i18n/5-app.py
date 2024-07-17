0c0da48bb0221022e772eacac7030b9749e7d7f2
Victor Mwendwa <victormwendwa804@gmail.com>
2024-07-17 17:39:10 +0300

    Mock logging in

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -22,7 +22,7 @@ class Config:
 
 app.config.from_object(Config)
 
-Users = {
+users = {
     1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
     2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
     3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
